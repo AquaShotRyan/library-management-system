708380dd1a2968cb91a07b706a1793940ebc24f4
Ryan <94146625+AquaShotRyan@users.noreply.github.com>
2025-10-02 11:29:42 -0400

    R-CODE-01

--- src/main/java/org/example/Catalogue.java
+++ src/main/java/org/example/Catalogue.java
@@ -9,8 +9,12 @@ public class Catalogue {
         catalogue = new ArrayList<Book>();
     }
 
+    public void addBook(Book book){
+        catalogue.add(book);
+    }
+
     Book getBook(int index){
-        return new Book("testBook", "testAuthor");
+        return catalogue.get(index);
     }
 
     public int getCatalogueSize(){
--- src/main/java/org/example/InitializeLibrary.java
+++ src/main/java/org/example/InitializeLibrary.java
@@ -5,6 +5,26 @@ public class InitializeLibrary {
     Catalogue catalogue = new Catalogue();
 
     public Catalogue initializeLibrary(){
+        catalogue.addBook(new Book("Great Gatsby", "F. Scott FitzGerald"));
+        catalogue.addBook(new Book("Red Rising", "Pierce Brown"));
+        catalogue.addBook(new Book("The Ways of Kings", "Brandon Sanderson"));
+        catalogue.addBook(new Book("A Game of Thrones", "George R.R. Martin"));
+        catalogue.addBook(new Book("The Handmaid's Tale", "Margaret Atwood"));
+        catalogue.addBook(new Book("Nineteen Eighty-Four", "George Orwell"));
+        catalogue.addBook(new Book("The Apothecary Diaries: Volume 1", "author7"));
+        catalogue.addBook(new Book("The Science of Beauty", "Michelle Wong"));
+        catalogue.addBook(new Book("No Longer Human", "Osamu Dazai"));
+        catalogue.addBook(new Book("Eragon", "Christopher Paolini"));
+        catalogue.addBook(new Book("Moby-Dick", "Herman Melville"));
+        catalogue.addBook(new Book("Supergirl: Woman of Tomorrow #1", "Tom King"));
+        catalogue.addBook(new Book("Absolute Batman #1", "Scott Snyder"));
+        catalogue.addBook(new Book("Berserk Deluxe Volume 1", "Kentaro Miura"));
+        catalogue.addBook(new Book("To Kill a Mockingbird", "Harper Lee"));
+        catalogue.addBook(new Book("The Art and Making of Arcane", "Elisabeth Vincentelli"));
+        catalogue.addBook(new Book("The Hunger Games", "Suzanne Collins"));
+        catalogue.addBook(new Book("Blood Meridian", "Cormac McCarthy"));
+        catalogue.addBook(new Book("Crime and Punishment", "Fyodor Dostoevsky"));
+        catalogue.addBook(new Book("2001: A Space Odyssey", "Arthur C. Clarke"));
         return catalogue;
     }
 }
