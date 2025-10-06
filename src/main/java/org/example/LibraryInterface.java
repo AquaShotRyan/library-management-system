5ff793a9945a0d926fbbfec57d347c175189e983
Ryan <94146625+AquaShotRyan@users.noreply.github.com>
2025-10-06 16:18:31 -0400

    R-TEST-03 RESP_03_test_1 RESP_03_test_2 RESP_03_test_3

---
+++ src/main/java/org/example/LibraryInterface.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class LibraryInterface {
+    public String promptStringInput(Scanner input, PrintWriter output, String prompt){
+        return "";
+    }
+}
