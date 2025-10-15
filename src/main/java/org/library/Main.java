1ed92da8dbdfb0dc15b8f20abfa156952ec1c8c4
Ryan <94146625+AquaShotRyan@users.noreply.github.com>
2025-10-15 14:37:45 -0400

    REFAC - delete Scanner input as attribute of LibraryController - add main functionality for RESP-09, asking the user to confirm book borrowing - add handling for wrong input for selecting to return book

--- src/main/java/org/library/Main.java
+++ src/main/java/org/library/Main.java
@@ -1,17 +1,15 @@
 package org.library;
 
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         // setup
         Library library = new Library();
         LibraryInterface ui = new LibraryInterface();
-        Scanner input = new Scanner(System.in);
         PrintWriter output = new PrintWriter(System.out);
 
-        LibraryController controller = new LibraryController(library, ui, input, output);
+        LibraryController controller = new LibraryController(library, ui, output);
 
         // main loop
         controller.run();
