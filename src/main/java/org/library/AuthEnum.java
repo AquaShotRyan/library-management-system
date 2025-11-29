e3647d71f0b9d443c14daeefc792b84f90e123b7
Ryan <94146625+AquaShotRyan@users.noreply.github.com>
2025-11-29 14:36:20 -0500

    Delete java code

--- src/main/java/org/library/AuthEnum.java
+++
@@ -1,7 +0,0 @@
-package org.library;
-
-public enum AuthEnum {
-    SUCCESS,
-    INVALID_CREDENTIALS,
-    INVALID_INPUT
-}
--- src/main/java/org/library/BookAuthorComparator.java
+++
@@ -1,9 +0,0 @@
-package org.library;
-
-import java.util.Comparator;
-
-public class BookAuthorComparator implements Comparator<Book> {
-    public int compare(Book a, Book b){
-        return a.getAuthor().compareTo(b.getAuthor());
-    }
-}
